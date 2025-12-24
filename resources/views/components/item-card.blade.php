609dd91062de4f296f42e2d67e4b0d6ae4c5452c
Ridwan Ihsan <ridwan.ihsann@gmail.com>
2025-12-24 11:48:02 +0700

    first commit

--- resources/views/components/item-card.blade.php
+++ resources/views/components/item-card.blade.php
@@ -3,12 +3,10 @@
 <a href="{{ route('items.show', $item->id) }}" class="item-card">
     <div class="item-image">
         @if($item->image)
-            <img src="{{ asset('storage/' . $item->image) }}" alt="{{ $item->name }}">
+            {{-- Menggunakan Storage::url adalah cara paling standar Laravel --}}
+            <img src="{{ Storage::url($item->image) }}" alt="{{ $item->name }}">
         @else
-            <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
-            </svg>
+            {{-- SVG Placeholder --}}
         @endif
     </div>
 
