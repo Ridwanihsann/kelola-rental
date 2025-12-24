25bbda556f7778b7167ae8b4f24bbe05037bf1a6
Ridwan Ihsan <ridwan.ihsann@gmail.com>
2025-12-24 10:21:28 +0700

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: touch database/database.sqlite && php artisan config:cache && php artisan migrate --force && php artisan db:seed --force && php artisan serve --host=0.0.0.0 --port=$PORT
---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AdminSeeder extends Seeder
+{
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@rental.com',
+            'password' => Hash::make('admin123'),
+        ]);
+    }
+}
