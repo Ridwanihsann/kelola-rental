0ebcacc286a910f373abb5cdf6796dfdc8d83e66
Ridwan Ihsan <ridwan.ihsann@gmail.com>
2025-12-24 11:32:31 +0700

    konfig

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Mempercayai semua proxy agar HTTPS berjalan lancar di Railway
         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
